24cb1805571ca460a8569b705c54a040a7c872f1
Andrew Phommathep <13destinies@gmail.com>
2016-11-21 23:16:26 -0800

    One instance

--- src/app/service/cmp-b.component.ts
+++ src/app/service/cmp-b.component.ts
@@ -22,7 +22,7 @@ import {DataService} from "./data.service";
         <p>{{value}}</p>
     </div>
   `,
-  providers: [LogService, DataService]
+  providers: [LogService]
 })
 export class CmpBComponent {
   value = '';
